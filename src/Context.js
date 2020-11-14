2663e6fb17ebfc3d6fb3227d69fb1b5a74bdceb2
Devashish <ranedevashish2@gmail.com>
2020-11-14 23:55:58 +0530

    Only cart functionality remaining

--- src/Context.js
+++ src/Context.js
@@ -12,7 +12,21 @@ class DataProvider extends Component {
         this.state = {
              products: [],
              detailProduct : detailProduct,
-             cart:[]
+             cart:[{
+                id: 1,
+                title: "Google Pixel - Black",
+                img: "img/product-1.png",
+                price: 10,
+                company: "GOOGLE",
+                info:
+                  "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+                inCart: false,
+                count: 0,
+                total: 0
+              }],
+             subtotal:0,
+             cartTax:0,
+             cartTotal:0
         }
     }
     componentDidMount(){
@@ -59,6 +73,22 @@ class DataProvider extends Component {
        return item;
     }
 
+    // methods related to cart
+    increment= (id)=>{
+        console.log("incr");
+    }
+    decrement = (id)=>{
+        console.log("decr");
+    }
+    removeItem = (id)=>{
+        console.log("Item removed");             
+    }
+    clearCart = ()=>{
+        this.setState(()=>{
+           return {cart:[]}
+        })
+    }
+
     
     render() {
     
@@ -66,7 +96,11 @@ class DataProvider extends Component {
         
             <DataContext.Provider value={{...this.state,
             addToCart:this.addToCart,
-            handleDetail:this.handleDetail}}>
+            handleDetail:this.handleDetail,
+            increment:this.increment,
+            decrement:this.decrement,
+            removeItem:this.removeItem,
+            clearCart:this.clearCart}}>
                 { this.props.children }
             </DataContext.Provider>
         )
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,11 +1,68 @@
 import React, { Component } from 'react'
 
+import {DataConsumer} from '../Context';
 export default class Cart extends Component {
     render() {
         return (
-            <div>
-                Cart
-            </div>
+            <>
+                
+              <DataConsumer>
+                 
+                    {
+                        (data)=>{
+                            console.log(data.increment);
+                            const {decrement,increment,removeItem,clearCart} = data
+                            if(data.cart.length!=0){
+                                    return(
+                                        <>
+                                        <div style={{display:"flex",alignItems:"center",justifyContent:"center"}}>
+                                        <h1 className="text-center" >Your Cart</h1>
+                                        <button onClick={()=>clearCart()} style={{}} class="btn btn-primary btn-lg ml-3">Clear Cart</button>
+                                        </div>
+                                        <div className="border border-primary mt-5"  >
+                                           
+
+                                            {
+                                                data.cart.map(inCartItem=>{
+                                                  return  <div className="row ">
+                                                    <div className="col-5 ml-4 sm-12">
+                                                        <img style={{width:"200px",height:"200px"}} src={inCartItem.img}></img>    
+                                                            
+                                                    </div>
+
+                                                    <div className="col-3 mt-5 sm-12">
+                                                            <h4>{inCartItem.title}</h4>
+                                                            <h4>{inCartItem.company}</h4>
+                                                    </div>
+
+                                                    <div className="col-3 mt-5 sm-12">
+                                                             <h4>{inCartItem.price}$</h4>
+                                                                 <div style={{display:"flex"}} >
+                                                                     <button className="btn btn-danger" onClick={()=>decrement(inCartItem.id)} > - </button>
+                                                                    <h4 className="mx-3"> {inCartItem.count} </h4>  
+                                                                    <button className="btn btn-success" onClick={()=>increment(inCartItem.id)} > + </button>
+                                                                    <button onClick={()=>removeItem(inCartItem.id)}  className=" ml-5 btn btn-outline-danger"> <i className="fa fa-trash-o" aria-hidden="true"></i> </button>
+                                                            </div>
+                                                    </div>
+                                                   </div>
+                                                })
+                                            }
+                                            
+                                            
+                                            
+                                        </div>
+                                    
+                                    </>) //first return end
+                                    }
+                                    else{
+                                            return(
+                                                <h2 className="text-center">Your cart is empty...</h2>
+                                            )                                           
+                                    }
+                        }
+                    }
+              </DataConsumer>
+            </>
         )
     }
 }
